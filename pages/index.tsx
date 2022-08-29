c4c91c5ef8a96c72e2da6d30e2d2081c847069a1
Jorge Rojas <jorge.otto.415@gmail.com>
2022-08-28 19:14:25 -0500

    Initial setup and config

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1>This is a test app</h1>
+      <h2>Where people can interact with a playlist in youtube</h2>
+    </div>
+  );
+};
+
+export default Home;
