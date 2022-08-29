a3de1c6e6a7adf2278369c521a5f50aac1ad87ad
Jorge Rojas <jorge.otto.415@gmail.com>
2022-08-28 19:49:45 -0500

    Landing page test styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,10 +5,20 @@ import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <h1>This is a test app</h1>
-      <h2>Where people can interact with a playlist in youtube</h2>
-    </div>
+    <>
+      <div className="flex flex-col justify-center text-center align-middle h-screen bg-slate-900">
+        <h1 className="text-5xl h-20">This is a test app</h1>
+        <div className="h-15" />
+        <h2 className="text-xl">
+          Where people can interact with a playlist in youtube
+        </h2>
+      </div>
+      <div className="flex flex-col justify-center text-center align-middle h-screen bg-gray-800">
+        <h1 className="text-5xl h-20">It is for rock and chicharrón</h1>
+        <div className="h-15" />
+        <h2 className="text-xl">But it's currently under construction.</h2>
+      </div>
+    </>
   );
 };
 
