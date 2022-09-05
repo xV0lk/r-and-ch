6d8ca90faa20730902d71011c02d8874e488657a
Jorge Rojas <jorge.otto.415@gmail.com>
2022-09-05 06:57:50 -0500

    fix landing bug

--- src/components/landing/BaseLanding.tsx
+++ src/components/landing/BaseLanding.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react';
 const BaseLanding: FC = () => {
   return (
     <div className="h-full overflow-auto">
-      <div className="mt-[calc(100%-10rem)] h-full marker:font-lato text-center align-middle px-12 mx-auto">
+      <div className="mt-[calc(100%-10rem)] marker:font-lato text-center align-middle px-12 mx-auto">
         <h1 className="text-4xl h-20 font-bold">This is a test app</h1>
         <div className="h-8" />
         <h2 className="text-xl">
