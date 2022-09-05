e8e3165b900911d85b6a1fd571bf22f456229473
Jorge Rojas <jorge.otto.415@gmail.com>
2022-09-05 06:09:15 -0500

    page layout improoved

--- src/components/Layout/pageLayout.tsx
+++ src/components/Layout/pageLayout.tsx
@@ -6,14 +6,14 @@ type Props = {
 };
 export const PageLayout: React.FC<Props> = ({ children }) => {
   return (
-    <div className="w-screen h-screen bg-slate-900 bg-gradient-to-t from-slate-900 to-darkgrad-2">
-      <div className="absolute top-0 left-0">
+    <div className="w-screen h-screen bg-gradient-to-t from-slate-900 to-darkgrad-2">
+      <div className="w-full h-20 border border-slate-900">
         <Header />
       </div>
-      <div className="absolute top-0 left-0 h-[calc(100%-8rem)]">
-        <div className="flex h-full">{children}</div>
+      <div className="h-[calc(100%-5rem-8rem)]">
+        <div className="flex w-screen h-full">{children}</div>
       </div>
-      <div className="absolute bottom-0 left-0 w-screen h-32 bg-none border border-white"></div>
+      <div className="absolute bottom-0 left-0 w-screen h-32 bg-none border border-slate-800"></div>
     </div>
   );
 };
--- src/components/landing/BaseLanding.tsx
+++ src/components/landing/BaseLanding.tsx
@@ -2,15 +2,15 @@ import { FC } from 'react';
 
 const BaseLanding: FC = () => {
   return (
-    <>
-      <div className="flex h-full flex-col font-lato justify-center text-center align-middle px-12">
+    <div className="h-full overflow-auto">
+      <div className="mt-[calc(100%-10rem)] h-full marker:font-lato text-center align-middle px-12 mx-auto">
         <h1 className="text-4xl h-20 font-bold">This is a test app</h1>
-        <div className="h-15" />
+        <div className="h-8" />
         <h2 className="text-xl">
           Where people can interact with a playlist in youtube
         </h2>
       </div>
-    </>
+    </div>
   );
 };
 
