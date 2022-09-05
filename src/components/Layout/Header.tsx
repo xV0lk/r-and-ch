e94e1e39e0ae50d14e47d07df198e1b4f422c135
Jorge Rojas <jorge.otto.415@gmail.com>
2022-09-05 06:09:22 -0500

    header with grid

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -3,10 +3,12 @@ import { SvgLogo } from '../SvgLogo';
 
 const Header: FC = () => {
   return (
-    <div className="translate-x-5 translate-y-5 inline-block rounded-full bg-yellow-600 font-bold">
-      <span className="px-2 font-lobs text-2xl">
-        <SvgLogo />
-      </span>
+    <div className="h-full grid grid-cols-5 content-center">
+      <div className="justify-self-center self-center inline-block rounded-full bg-yellow-600 font-bold">
+        <span className="px-2 font-lobs text-2xl">
+          <SvgLogo />
+        </span>
+      </div>
     </div>
   );
 };
