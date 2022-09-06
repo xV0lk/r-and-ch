bf406eece1a4051b7ff4c091ddeb9fd92e2b1f13
Jorge Rojas <jorge.otto.415@gmail.com>
2022-09-06 08:23:23 -0500

    responsive styles

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -3,8 +3,8 @@ import { SvgLogo } from '../SvgLogo';
 
 const Header: FC = () => {
   return (
-    <div className="h-full grid grid-cols-5 content-center">
-      <div className="justify-self-center self-center inline-block rounded-full bg-yellow-600 font-bold">
+    <div className="h-full grid grid-cols-5 auto-cols-max content-center">
+      <div className="justify-self-center self-center inline-block rounded-full bg-yellow-600 font-bold sm:justify-self-start sm:ml-4">
         <span className="px-2 font-lobs text-2xl">
           <SvgLogo />
         </span>
--- src/components/landing/BaseLanding.tsx
+++ src/components/landing/BaseLanding.tsx
@@ -2,9 +2,11 @@ import { FC } from 'react';
 
 const BaseLanding: FC = () => {
   return (
-    <div className="h-full overflow-auto">
-      <div className="mt-[calc(100%-10rem)] marker:font-lato text-center align-middle px-12 mx-auto">
-        <h1 className="text-4xl h-20 font-bold">This is a test app</h1>
+    <div className="h-full w-full overflow-auto">
+      <div className="mt-[30vh] text-center align-middle px-12 mx-auto">
+        <h1 className="text-4xl font-lato h-20 font-bold">
+          This is a test app
+        </h1>
         <div className="h-8" />
         <h2 className="text-xl">
           Where people can interact with a playlist in youtube
