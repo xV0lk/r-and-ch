fd5fb4759888eaed8358d57f0fec28af005aa81c
Jorge Rojas <jorge.otto.415@gmail.com>
2022-09-07 07:16:10 -0500

    RC logo in header

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,13 +1,18 @@
+import Image from 'next/image';
 import { FC } from 'react';
 import { SvgLogo } from '../SvgLogo';
+import { LogoSimple } from '../../../public/logo-simple';
 
 const Header: FC = () => {
   return (
-    <div className="h-full grid grid-cols-5 auto-cols-max content-center">
-      <div className="justify-self-center self-center inline-block rounded-full bg-yellow-600 font-bold sm:justify-self-start sm:ml-4">
+    <div className="h-full grid grid-cols-5 auto-cols-max">
+      {/* <div className="justify-self-center self-center inline-block rounded-full bg-orange-1 font-bold sm:justify-self-start sm:ml-4">
         <span className="px-2 font-lobs text-2xl">
           <SvgLogo />
         </span>
+      </div> */}
+      <div className="col-start-1 col-span-2 place-self-center inline-block sm:justify-self-start sm:ml-8">
+        <LogoSimple height={65} />
       </div>
     </div>
   );
