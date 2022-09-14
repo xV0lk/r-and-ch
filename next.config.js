b8211cd82d8b1e21b7b917246237cd2ee02e9006
Jorge Rojas <jorge.otto.415@gmail.com>
2022-09-14 06:56:13 -0500

    upgrade to yarn

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  ...nextConfig,
+  experimental: {
+    forceSwcTransforms: true,
+  },
+};
