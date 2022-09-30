69051c0d1ece60e6ed5ffdd6c4cba0464c22773f
Jorge ROJAS <jrojas@ikatechsolutions.com>
2022-09-30 07:46:13 -0500

    add react query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,19 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { PageLayout } from '../components/Layout/pageLayout'
+
+const queryClient = new QueryClient()
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <PageLayout>
-      <Component {...pageProps} />
-    </PageLayout>
+    <QueryClientProvider client={queryClient}>
+      <PageLayout>
+        <Component {...pageProps} />
+      </PageLayout>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
 
