1cd48d9f487594601d9015546a020c29a7939c8e
Jorge Rojas <jorge.otto.415@gmail.com>
2022-10-01 07:27:55 -0500

    auth

---
+++ src/pages/signin.tsx
@@ -0,0 +1,9 @@
+import AuthForm from '../components/landing/AuthForm'
+
+const Signin = () => {
+  return <AuthForm mode="signin" />
+}
+
+Signin.authPage = true
+
+export default Signin
---
+++ src/pages/signup.tsx
@@ -0,0 +1,7 @@
+import AuthForm from '../components/landing/AuthForm'
+
+const Signup = () => {
+  return <AuthForm mode="signup" />
+}
+
+export default Signup
